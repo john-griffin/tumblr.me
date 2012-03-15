c7c7a0d732dd2854515af80a9d82ef9a241b610a
John Griffin <johnog@gmail.com>
2012-03-15 21:58:57 +0000

    pass the through the name of any tumblr

--- tumblrme.rb
+++ tumblrme.rb
@@ -4,8 +4,8 @@ Bundler.require(:default)
 require "active_support/cache"
 
 class Tumblrme < Sinatra::Base
-  def tumblrs
-    connection = Faraday.new(:url => 'http://api.tumblr.com/v2/blog/kimjongillookingatthings.tumblr.com/posts/photo') do |conn|
+  def tumblrs(name)
+    connection = Faraday.new(:url => "http://api.tumblr.com/v2/blog/#{name}.tumblr.com/posts/photo") do |conn|
       conn.response :rashify
       conn.response :json, :content_type => /\bjson$/
       conn.response :caching do
@@ -14,7 +14,7 @@ class Tumblrme < Sinatra::Base
       conn.adapter Faraday.default_adapter
     end
     response = connection.get do |req|
-      req.params["api_key"] = 'qpvITi2QuD3We7q6iz9ofLGVYLAVZ64g2XK5p7aZwcJ0KSg5nf'
+      req.params["api_key"] = ENV["API_KEY"]
       req.params["limit"]   = '200'
     end
     response.body.response.posts.map do |post|
@@ -30,11 +30,11 @@ class Tumblrme < Sinatra::Base
     content_type :json
   end
 
-  get '/' do
-    tumblrs.to_json
+  get '/:name' do
+    tumblrs(params[:name]).to_json
   end
 
-  get '/random' do
-    tumblrs.sample.to_json
+  get '/:name/random' do
+    tumblrs(params[:name]).sample.to_json
   end
 end
